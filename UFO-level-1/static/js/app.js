62925177a091de8ebf72dcb7d9ef90a982ae5c63
DongYi Kim <waterddong91@gmail.com>
2021-01-15 16:57:49 -0500

    ufo level one_some changes
    change from Var to function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,25 +5,28 @@ var tableData = data;
 var tbody = d3.select("tbody");
 console.log(tableData);
 
-var ufodata = (rowdata ) => {
+// Create function to create table
+function ufodata(rowdata) {
     tbody.html("");
+    //Loop through 'data' 
     rowdata.forEach((ufoReport) => {
+        //Append the table row 'tr
         var row = tbody.append("tr");
+        //Use Object.entries to ceach UFO report value
         Object.entries(ufoReport).forEach(([key, value]) => {
+            // Append one cell per UFO report value
             var cell = row.append("td");
+            // Update each cells text with UFO report value
             cell. text(value);
         });
     });
 };
+// Build and show table
 ufodata(tableData);
-
-//---------------------------------------------------------
 
 
 // Select the button
 var button = d3.select("#filter-btn");
-// Select the form
-//var form = d3.select(".filters");
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -34,13 +37,16 @@ function runEnter() {
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
  
-    console.log(inputValue);
-    console.log(tableData);
+    //console.log(inputValue);
+    //console.log(tableData);
 
+    // Get the filtered data and console.log filteredData
     var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-
     console.log(filteredData);
+    
+    //// Build and show table
     ufodata(filteredData);  
-
 };
+
+// Create on function to attach an even to the runEnter function
 button.on("click", runEnter);
